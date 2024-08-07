641b369c2fd0995b7d534d0e0f3bef8cdf5a8749
Gui-gt <ggk0830755@gmail.com>
2024-08-06 23:54:43 -0300

    atualização de código
    o que eu aprendi hoje sobre o sistema de estoque.

--- exercicios/código c#/Sistema-de-estoque/Program.cs
+++ exercicios/código c#/Sistema-de-estoque/Program.cs
@@ -8,8 +8,37 @@ namespace Sistema_de_estoque
 {
     internal class Program
     {
+
+        enum Menu { listagem = 1, adicionar, remover, entrada, saida, sair };
+
         static void Main(string[] args)
         {
+
+            bool escolheuSair = false;
+
+            Console.WriteLine("<<  Sistema de Estoque  >>");
+            Console.WriteLine("\n1-listagem de produtos\n2-adicionar produto\n3-remover produto\n4-entrada de produtos\n5-saida de produtos\n6-sair");
+            string instr = Console.ReadLine();
+            int intop = int.Parse(Console.ReadLine());
+            Menu opcao = (Menu)intop;
+
+            switch(opcao)
+            {
+                case Menu.listagem:
+                    break;
+                case Menu.adicionar:
+                    break;
+                case Menu.remover:
+                    break;
+                case Menu.entrada:
+                    break;
+                case Menu.saida:
+                    break;
+                case Menu.sair:
+                    break;  
+
+            }
+
         }
     }
 }
