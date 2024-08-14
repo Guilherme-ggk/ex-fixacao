79a1e623bb6b5ee67b9fd4d74423175937e9b661
Gui-gt <ggk0830755@gmail.com>
2024-08-13 21:53:11 -0300

    atualização de código

---
+++ exercicios/sistema de estoque #2/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static List<Iestoque> produtos = new List<Iestoque>();
+        enum Menu { listagem = 1, adicionar, remover, entrada, saida, sair};
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
