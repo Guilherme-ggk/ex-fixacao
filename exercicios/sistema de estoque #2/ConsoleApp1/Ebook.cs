dda5d779be0485d532069a9e8876a5075a1529ed
Gui-gt <ggk0830755@gmail.com>
2024-08-13 22:43:34 -0300

    Finalizando por hoje

---
+++ exercicios/sistema de estoque #2/ConsoleApp1/Ebook.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Ebook : Produto, Iestoque
+    {
+        public string autor;
+        private int vendas;
+
+        public Ebook(string nome, float preco, string autor)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.autor = autor;
+        }
+
+        public void Entrada()
+        {
+            Console.WriteLine("Não é possivel adicionar entrada em Ebook, produto digital!");
+        }
+
+        public void Exibir()
+        {
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Preço: {preco}");
+            Console.WriteLine($"Autor: {autor}");
+            Console.WriteLine("==============================");
+        }
+
+        public void Saida()
+        {
+            Console.WriteLine("Quanto de vendas quer dar saída?");
+            int saida = int.Parse( Console.ReadLine() );
+            vendas += saida;
+            Console.WriteLine("Vendas registradas");
+        }
+    }
+}
--- exercicios/sistema de estoque #2/ConsoleApp1/Program.cs
+++ exercicios/sistema de estoque #2/ConsoleApp1/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +15,8 @@ namespace ConsoleApp1
         enum Menu { listagem = 1, adicionar, remover, entrada, saida, sair};
         static void Main(string[] args)
         {
+            Carregar();
+
             bool escolheuSair = false;
 
             while (!escolheuSair)
@@ -27,6 +32,7 @@ namespace ConsoleApp1
                     switch (escolha)
                     {
                         case Menu.listagem:
+                            Listagem();
                             break;
                         case Menu.adicionar:
                             break;
@@ -45,6 +51,51 @@ namespace ConsoleApp1
                 }
                 Console.Clear();
             }
+        }
+
+        static void Listagem()
+        {
+            if(produtos.Count > 0)
+            {
+                foreach (Iestoque produto in produtos)
+                {
+                    produto.Exibir();
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nenhum produto cadastrado!");
+            }
+            Console.ReadLine();
+        }
+        static void Salvar()
+        {
+            FileStream stream = new FileStream("produtos.dat",FileMode.OpenOrCreate);
+            BinaryFormatter encoder = new BinaryFormatter();
+
+            encoder.Serialize(stream, produtos);
+
+            stream.Close();
+        }
+        static void Carregar()
+        {
+            FileStream stream = new FileStream("produtos.dat", FileMode.OpenOrCreate);
+            try
+            {
+                BinaryFormatter encoder = new BinaryFormatter();
+                produtos = (List<Iestoque>)encoder.Deserialize(stream);
+                if(produtos == null)
+                {
+                    produtos = new List<Iestoque>();
+                }
+
+            }
+            catch (Exception)
+            {
+                produtos = new List<Iestoque>();
+            }
+            stream.Close();
         }
     }
 }
