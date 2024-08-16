179560aa75b40420ccfdfd148b419c3cf90fc66f
Guilherme Henrique <ggk0830755@gmail.com>
2024-08-15 22:16:45 -0300

    atualização do Produto

--- exercicios/código c#/Sistema-de-estoque/Produto.cs
+++ exercicios/código c#/Sistema-de-estoque/Produto.cs
@@ -6,10 +6,8 @@ using System.Threading.Tasks;
 
 namespace Sistema_de_estoque
 {
-    [System.Serializable]
+    
     class Produto
     {
-        public string nome;//classe pai que vai passar esses dois metodos para os filhos
-        public float preco;
     }
 }
