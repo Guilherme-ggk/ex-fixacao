8e2cd5dc3b61bc889eaecd76ce686d4b8d02001f
Guilherme Henrique <ggk0830755@gmail.com>
2024-08-15 22:15:46 -0300

    atualização do ebook

--- exercicios/código c#/Sistema-de-estoque/Ebook.cs
+++ exercicios/código c#/Sistema-de-estoque/Ebook.cs
@@ -6,43 +6,9 @@ using System.Threading.Tasks;
 
 namespace Sistema_de_estoque
 {
-    [System.Serializable]
-    class Ebook : Produto ,IEstoque
+   
+    class Ebook
     {
-        public string autor;
-        private int vendas;
-
-        public Ebook(string nome, float preco, string autor)
-        {
-            this.nome = nome;
-            this.preco = preco;
-            this.autor = autor;
-        }
-
-        public void AdicionarEntrada()
-        {
-           Console.WriteLine("Não é possivel dar entrada em ebook, pois é um produto digital!");
-            Console.ReadLine();//produtos digitais como ebooks e jogos e filmes, são contabilizados somente a venda
-        }
-
-        public void AdicionarSaida()
-        {
-            Console.WriteLine($"Adicionando saida de vendas no ebook {nome}:");
-            Console.WriteLine("Digite a quantidade de vendas que você quer dar saida:");
-            int entrada = int.Parse(Console.ReadLine());
-            //estoque = estoque + entrada; mesma coisa
-            vendas += entrada;
-            Console.WriteLine("Vendas registradas com sucesso!");
-            Console.ReadLine();
-        }
-
-        public void Exibir()
-        {
-            Console.WriteLine($"Nome: {nome}");
-            Console.WriteLine($"Autor: {autor}");
-            Console.WriteLine($"Preço: {preco}");
-            Console.WriteLine($"Vagas restantes: {vendas}");
-            Console.WriteLine("===================================");
-        }
+       
     }
 }
