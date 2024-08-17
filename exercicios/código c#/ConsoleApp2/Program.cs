8dcb3239867b04e94de6bf1798ad54ac1df78b95
Gui-gt <ggk0830755@gmail.com>
2024-08-16 21:50:06 -0300

    Criando mais um sistema

---
+++ exercicios/código c#/ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        enum Menu
+        {
+            listagem=1,
+            cadastrar,
+            remover, 
+            entrada,
+            saida,
+            sair
+        }
+
+        static List<>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   <<<Sistema de estoque >>>");
+            Console.WriteLine("\n1-listagem\n2-cadastrar\n3-remover\n4-entrada\n5-saida\n6-sair");
+            string str = Console.ReadLine();
+            int intop = int.Parse(str);
+
+        }
+    }
+}
--- exercicios/código c#/ConsoleApp2/Properties/AssemblyInfo.cs
+++ exercicios/código c#/ConsoleApp2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // As informações gerais sobre um assembly são controladas por
 // conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associadas a um assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("ConsoleApp2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("ConsoleApp2")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
-[assembly: Guid("288dc2ae-45f2-44e5-98f1-876f116ce615")]
+[assembly: Guid("951bde30-e568-4728-8f35-55c8e758b6ea")]
 
 // As informações da versão de um assembly consistem nos quatro valores a seguir:
 //
