312878b139a6b6956da39f96351fcd3c9c807f61
Gui-gt <ggk0830755@gmail.com>
2024-08-16 22:06:12 -0300

    mexendo em outro estoque

---
+++ exercicios/sistema de estoque #2/ConsoleApp1/ProdutoFisico.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class ProdutoFisico : Produto, IEstoque
+    {
+        public float frete;
+        private int estoque;
+
+        public ProdutoFisico(float frete)
+        {
+            this.frete = frete;
+        }
+
+        public void AdicionarEntrada()
+        {
+            Console.WriteLine($"Quanto de estoque deseja dar entrada no produto {nome}?");
+            int valor = int.Parse(Console.ReadLine());
+            estoque += valor;
+            Console.WriteLine("Resgitrado!");
+            Console.ReadLine();
+
+        }
+
+        public void AdicionarSaida()
+        {
+            Console.WriteLine($"Quanto de estoque deseja dar saída no produto {nome}?");
+            int valor = int.Parse(Console.ReadLine());
+            estoque -= valor;
+            Console.WriteLine("Resgitrado!");
+            Console.ReadLine();
+        }
+
+        public void Exibir()
+        {
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Preço: {preco}");
+            Console.WriteLine($"Frete: {frete}");
+            Console.WriteLine($"Estoque: {estoque}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ exercicios/sistema de estoque #2/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static List<IEstoque>  produtos = new List<IEstoque>();
+        enum Menu { listagem = 1, cadastrar, remover, entrada, saida, sair };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<<Sistema de estoque>>");
+            Console.WriteLine();
+        }
+    }
+}
