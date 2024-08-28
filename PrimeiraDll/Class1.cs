ca7be70c383ae2e1b4003f59379cdc36e9382e0f
Gui-gt <ggk0830755@gmail.com>
2024-08-27 22:38:01 -0300

    att

--- PrimeiraDll/Class1.cs
+++ PrimeiraDll/Class1.cs
@@ -13,6 +13,7 @@ namespace PrimeiraDll
         //nessa dll vou usar a API do WhatsApp business para enviar pdfs direto para o cliente
         //mudança de planos, agora só vou tentar criar um código em vb6 para fazer o envio do pdf
         //ret = ShellExecute(Me.hWnd, "OPen", "https://api.whatsapp.com/send?phone=" & "+55" + "43996960000" & "&text=" & Replace("teste", " ", "%20"), "", "", 1)
+        //Dim ret&
 
     }
 }
