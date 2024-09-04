1d712bfc86ee2d8fcca0853a76bfb32b1f30a5b6
Gui-gt <ggk0830755@gmail.com>
2024-09-04 20:14:43 -0300

    executando a api

--- MinhaPrimeiraAPIC#/obj/Debug/net8.0/MinhaPrimeiraAPIC#.AssemblyInfo.cs
+++ MinhaPrimeiraAPIC#/obj/Debug/net8.0/MinhaPrimeiraAPIC#.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinhaPrimeiraAPIC#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c03ff537abdd44f493aeab0b05c8e5c6d999acf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c53eaeabf64b6a31aeb690329664bfb999b21507")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinhaPrimeiraAPIC#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinhaPrimeiraAPIC#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
