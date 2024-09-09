8f102e4b962a43bf94d39cf469f7a700b5718326
Gui-gt <ggk0830755@gmail.com>
2024-09-09 11:17:46 -0300

    fazendo calculadora no console

--- Projetos Portfólio/Calculadora_port/Program.cs
+++ Projetos Portfólio/Calculadora_port/Program.cs
@@ -8,11 +8,43 @@ namespace Calculadora_port
 {
     internal class Program
     {
+        enum Menu { soma = 1, sub, div, mult, pot, raiz, sair };
         static void Main(string[] args)
         {
-            Console.WriteLine(" <<< Calculadora >>>");
-            Console.WriteLine("1-Soma\n2-Subtração\n3-Divisão\n4-Multiplicação\n5-Potência\n6-Raiz\n7-Sair");
+            bool escolheuSair = false;
 
+            while (escolheuSair == false)
+            {
+                Console.WriteLine(" <<< Calculadora >>>");
+                Console.WriteLine("1-Soma\n2-Subtração\n3-Divisão\n4-Multiplicação\n5-Potência\n6-Raiz\n7-Sair");
+
+                Menu opcao = (Menu)int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case Menu.soma:
+                        Soma();
+                        break;
+                    case Menu.sub:
+                        break;
+                    case Menu.div:
+                        break;
+                    case Menu.mult:
+                        break;
+                    case Menu.pot:
+                        break;
+                    case Menu.raiz:
+                        break;
+                    case Menu.sair:
+                        escolheuSair = true;
+                        break;
+
+                }
+            }
+        }
+        static void Soma()
+        {
+            Console.WriteLine("");
         }
     }
 }
