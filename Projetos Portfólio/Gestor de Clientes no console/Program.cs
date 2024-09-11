864652e3e583c3fab89b59d1723c0ccccaae0ff1
Gui-gt <ggk0830755@gmail.com>
2024-09-11 10:18:30 -0300

    atualização

---
+++ Projetos Portfólio/Gestor de Clientes no console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestor_de_Clientes_no_console
+{
+    internal class Program
+    {
+        enum Menu { listagem = 1, adicionar, remover, sair };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gestor de Clientes");
+            Console.WriteLine("");
+        }
+    }
+}
