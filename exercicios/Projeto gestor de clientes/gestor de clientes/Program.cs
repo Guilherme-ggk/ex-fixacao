cef4b1ae5b3a652daf05e24fc1bfe87575ccfa0d
Gui-gt <ggk0830755@gmail.com>
2024-09-12 11:51:13 -0300

    att

--- exercicios/Projeto gestor de clientes/gestor de clientes/Program.cs
+++ exercicios/Projeto gestor de clientes/gestor de clientes/Program.cs
@@ -8,8 +8,39 @@ namespace gestor_de_clientes
 {
     internal class Program
     {
+        struct Clientes
+        {
+            public string nome;
+            public int idade;
+        }
+        static List<Clientes> clientes = new List<Clientes>();
+
+        enum Menu { lista = 1, adicionar, remover, sair};
         static void Main(string[] args)
         {
+            bool escolheuSair = false;
+            while (escolheuSair == false)
+            {
+                Console.WriteLine("Gestor de Clientes");
+                Console.WriteLine("1-listagem\n2-adicionar\n3-remover\n4-sair");
+                int intop = int.Parse(Console.ReadLine());
+                Menu opcao = (Menu)intop;
+
+                switch (opcao)
+                {
+                    case Menu.lista:
+                        break;
+                    case Menu.adicionar:
+                        break;
+                    case Menu.remover:
+                        break;
+                    case Menu.sair:
+                        escolheuSair = true;
+                        break;
+                }
+            }
+
+            
         }
     }
 }
