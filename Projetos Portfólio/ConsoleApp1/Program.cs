2fc2a8021d12579754b32d262904abedbbe53816
Gui-gt <ggk0830755@gmail.com>
2024-09-20 22:06:02 -0300

    att

--- Projetos Portfólio/ConsoleApp1/Program.cs
+++ Projetos Portfólio/ConsoleApp1/Program.cs
@@ -8,10 +8,23 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+        enum Menu { lista = 1, cadastrar, remover, entrada, saida, sair};
+
+        
+
         static void Main(string[] args)
         {
+            bool escolheuSair = false;
+            while (escolheuSair == false)
+            {
+
+            }
             Console.WriteLine("0<Gestor de Produtos >0");
             Console.WriteLine("1-listagem\n2-cadastrar\n3-remover\n4-entrada\n5-saída\n6-sair");
+            int intop = int.Parse(Console.ReadLine());
+            Menu opcao = (Menu)intop;
+
+
 
         }
     }
