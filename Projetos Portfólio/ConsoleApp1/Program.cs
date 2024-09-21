490f607dd5bedd2a55e8a468fb9449c3240aa3c5
Gui-gt <ggk0830755@gmail.com>
2024-09-20 22:16:09 -0300

    att

--- Projetos Portfólio/ConsoleApp1/Program.cs
+++ Projetos Portfólio/ConsoleApp1/Program.cs
@@ -14,18 +14,32 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
-            bool escolheuSair = false;
-            while (escolheuSair == false)
+            bool EscolheuSair = false;
+            while (EscolheuSair == false)
             {
-
+                Console.WriteLine("0<Gestor de Produtos >0");
+                Console.WriteLine("1-listagem\n2-cadastrar\n3-remover\n4-entrada\n5-saída\n6-sair");
+                int intop = int.Parse(Console.ReadLine());
+                Menu opcao = (Menu)intop;
+               
+                    switch (opcao)
+                    {
+                        case Menu.lista:
+                            break;
+                        case Menu.cadastrar:
+                            break;
+                        case Menu.remover:
+                            break;
+                        case Menu.entrada:
+                            break;
+                        case Menu.saida:
+                            break;
+                        case Menu.sair:
+                            EscolheuSair = true;
+                            break;
+                    }
+                Console.Clear();
             }
-            Console.WriteLine("0<Gestor de Produtos >0");
-            Console.WriteLine("1-listagem\n2-cadastrar\n3-remover\n4-entrada\n5-saída\n6-sair");
-            int intop = int.Parse(Console.ReadLine());
-            Menu opcao = (Menu)intop;
-
-
-
         }
     }
 }
