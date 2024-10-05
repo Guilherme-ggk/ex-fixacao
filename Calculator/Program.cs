8c7e2f0866d1b27d5c79fdb7f9f4d36a43d10c37
Gui-gt <ggk0830755@gmail.com>
2024-10-05 10:29:32 -0300

    finalizando

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -22,16 +22,22 @@ namespace Calculator
                 switch (opcao)
                 {
                     case Menu.soma:
+                        Soma();
                         break;
                     case Menu.sub:
+                        Sub();
                         break;
                     case Menu.mult:
+                        Mult();
                         break;
                     case Menu.div:
+                        Div();
                         break;
                     case Menu.pot:
+                        Pot();
                         break;
                     case Menu.raiz:
+                        Raiz();
                         break;
                     case Menu.sair:
                         EscolheuSair = true;
@@ -77,6 +83,24 @@ namespace Calculator
             Console.WriteLine("Digite o segundo número:");
             int n2 = int.Parse(Console.ReadLine());
             int r = n1 / n2;
+            Console.WriteLine(r);
+            Console.ReadKey();
+        }
+        static void Pot()
+        {
+            Console.WriteLine("Digite o número base:");
+            int b1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o número expoente:");
+            int e2 = int.Parse(Console.ReadLine());
+            int r = (int)Math.Pow(b1, e2);
+            Console.WriteLine(r);
+            Console.ReadKey();
+        }
+        static void Raiz()
+        {
+            Console.WriteLine("Digite o número:");
+            int n = int.Parse(Console.ReadLine());
+            int r = (int)Math.Sqrt(n);
             Console.WriteLine(r);
             Console.ReadKey();
         }
