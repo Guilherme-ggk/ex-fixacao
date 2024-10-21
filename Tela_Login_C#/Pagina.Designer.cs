e5520b45799dabb3319dfcfeee2ae1921421a2fc
Gui-gt <ggk0830755@gmail.com>
2024-10-21 19:58:12 -0300

    att

--- Tela_Login_C#/Pagina.Designer.cs
+++ Tela_Login_C#/Pagina.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Pagina
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Pagina";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Pagina";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
