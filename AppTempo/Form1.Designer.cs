e19deb8baf42cbd0b7cd574c0e0551deeedb477e
Gui-gt <ggk0830755@gmail.com>
2024-11-02 20:03:31 -0300

    App de previsão do tempo

---
+++ AppTempo/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace AppTempo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnFechar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnFechar
+            // 
+            this.BtnFechar.BackColor = System.Drawing.Color.LightCoral;
+            this.BtnFechar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnFechar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BtnFechar.Location = new System.Drawing.Point(771, -2);
+            this.BtnFechar.Name = "BtnFechar";
+            this.BtnFechar.Size = new System.Drawing.Size(30, 23);
+            this.BtnFechar.TabIndex = 0;
+            this.BtnFechar.Text = "X";
+            this.BtnFechar.UseVisualStyleBackColor = false;
+            this.BtnFechar.Click += new System.EventHandler(this.BtnFechar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnFechar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnFechar;
+    }
+}
+
