88ed910838a0a010c6205675f5ab72744f9e5030
Gui-gt <ggk0830755@gmail.com>
2024-11-03 12:47:26 -0300

    att

--- AppTempo/Form1.Designer.cs
+++ AppTempo/Form1.Designer.cs
@@ -68,6 +68,7 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "entrar";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // pictureBox1
             // 
--- AppTempo/Pagina1.cs
+++ AppTempo/Pagina1.cs
@@ -10,29 +10,16 @@ using System.Windows.Forms;
 
 namespace AppTempo
 {
-    public partial class Form1 : Form
+    public partial class Pagina1 : Form
     {
-        public Form1()
+        public Pagina1()
         {
             InitializeComponent();
         }
-        private void pictureBox2_Click(object sender, EventArgs e)
-        {
-            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
-           
-        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             this.Close();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            var pagina = new Pagina1();
-
-            pagina.Show();
-
-         
         }
     }
 }
