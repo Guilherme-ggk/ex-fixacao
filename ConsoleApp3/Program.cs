397cff05b01c87523daba0535e9e58e0d8c4cbc6
Gui-gt <ggk0830755@gmail.com>
2024-11-06 11:42:12 -0300

    att

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +13,11 @@ namespace ConsoleApp3
     {
         struct Cliente
         {
-
+            public string nome;
+            public string idade;
         }
+
+        static List<Cliente> clientes = new List<Cliente>();
 
         enum Menu { listagem = 1, adicionar, remover, sair };
         static void Main(string[] args)
@@ -30,12 +36,62 @@ namespace ConsoleApp3
                     case Menu.adicionar:
                         break;
                     case Menu.remover:
+                        Remover();
                         break;
                     case Menu.sair:
                         EscolheuSair = true;
                         break;
                 }
             }
+        }
+
+
+
+        static void Remover()
+        {
+            Console.WriteLine("Qual cliente deseja remover?(id)");
+            int id = int.Parse(Console.ReadLine());
+            if (id >= 0 && clientes.Count > id)
+            {
+                clientes.RemoveAt(id);
+                Salvar();
+            }
+        }
+            
+        
+
+
+
+
+
+        static void Salvar()
+        {
+            FileStream stream = new FileStream("clientes.dat", FileMode.OpenOrCreate);
+            BinaryFormatter encoder = new BinaryFormatter();
+
+            encoder.Serialize(stream, clientes);
+
+            stream.Close();
+        }
+        static void Carregar()
+        {
+            FileStream stream = new FileStream("clientes.dat", FileMode.OpenOrCreate);
+            try
+            {
+                BinaryFormatter encoder = new BinaryFormatter();
+
+                clientes = (List<Cliente>)encoder.Deserialize(stream);
+
+                if(clientes == null)
+                {
+                    clientes = new List<Cliente>();
+                }
+            }
+            catch (Exception)
+            {
+                clientes = new List<Cliente>();
+            }
+            stream.Close();
         }
     }
 }
