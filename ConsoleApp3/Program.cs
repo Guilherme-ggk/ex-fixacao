f446e36c039a3793f72b1a5de38d89da72a2f918
Gui-gt <ggk0830755@gmail.com>
2024-11-07 06:53:57 -0300

    criando um gestor de  produtos

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -11,9 +11,41 @@ namespace ConsoleApp3
 {
     internal class Program
     {
+        enum Menu { listagem = 1, cadastro, remover, saida, entrada, sair };
         static void Main(string[] args)
         {
+            bool EscolheuSair = false;
+            while (EscolheuSair == false)
+            {
+                Console.WriteLine("Gestor de produtos");
+                Console.WriteLine("1-listagem de produtos\n2-cadastrar produtos\n3-remover\n4-adicionar saída\n5-adicionar entrada\n6-sair");
+                int intop = int.Parse(Console.ReadLine());
+                Menu opcao = (Menu)intop;
 
+                if (intop > 0 && intop < 7)
+                {
+                    switch (opcao)
+                    {
+                        case Menu.listagem:
+                            break;
+                        case Menu.cadastro:
+                            break;
+                        case Menu.remover:
+                            break;
+                        case Menu.saida:
+                            break;
+                        case Menu.entrada:
+                            break;
+                        case Menu.sair:
+                            EscolheuSair = true;
+                            break;
+                    }
+                }
+                else
+                {
+                    EscolheuSair = true;
+                }
+            }
         }
     }
 }
