8daee80cdd98c29656373eadcca265bb5ee6393b
Gui-gt <ggk0830755@gmail.com>
2025-01-17 11:47:15 -0300

    att

--- AppTempo/Properties/WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ AppTempo/Properties/WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3eb0166a0d4d91a6f9af755e532ac2ac817ec4d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ebf16659300be6834a1fdbd6d563e9149b1ee84")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
